790a19ccfeebd53814c2ad2ac4b132bc0c32068e
Vignesh227 <96369223+Vignesh227@users.noreply.github.com>
2023-08-29 23:32:16 +0530

    Made the website responsive to mobile devices

--- static/js/recommendation.js
+++ static/js/recommendation.js
@@ -2,7 +2,27 @@ const optionElements = document.querySelectorAll('option');
 
 Array.from(optionElements).forEach(element => {
   if (element.textContent.length > 35) {
-    element.textContent =
-      element.textContent.slice(0, 55) + '...';
+    element.textContent = element.textContent.slice(0, 55) + '...';
   }
 });
+
+/* Function to reduce the select options Width 
+automatically when device width less than 1000
+*/
+function checkDeviceWidth() {
+  if (window.innerWidth < 1000) {
+
+    Array.from(optionElements).forEach(element => {
+      if (element.textContent.length > 20) {
+        element.textContent = element.textContent.slice(0, 20) + '...';
+      }
+    });
+
+  }
+}
+
+// Run the function initially
+checkDeviceWidth();
+
+// Add an event listener to run the function whenever the window is resized
+window.addEventListener('resize', checkDeviceWidth);
